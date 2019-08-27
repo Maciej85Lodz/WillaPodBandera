f4cb17dc1864950df459563f78928682f5ac52b5
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-08-27 23:32:24 +0200

    Add gallery, link sections to nav.

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,3 +13,20 @@ selectElement('.open').addEventListener('click', () =>{
 selectElement('.close').addEventListener('click', () =>{
     selectElement('.nav-list').classList.remove('active');
 });
+
+//Gallery
+
+const msnry = new Macy({
+    container: '.gallery',
+    mobileFirst: true,
+    columns: 1,
+    breakAt: {
+      450: 2,
+      700: 3,
+      1100: 4,
+    },
+    margin: {
+      x: 20,
+      y: 20,
+    }
+  })
